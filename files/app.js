1967ce8f64292d511556bb35ff33c8b311fd91b1
Sachin Sharma <sachinmodgil6@gmail.com>
2022-01-28 15:58:45 +0530

    some minor changes

---
+++ files/app.js
@@ -0,0 +1,8 @@
+const signupbtn = document.getElementById("signupBTN");
+const signupBOX = document.getElementById("loginform");
+signupbtn.addEventListener("click", signupBOX);
+
+
+function signupBOX(){
+    alert("SIGN UP CLICKED");
+}
