540737784c74a17d91c39ca0478e22d08e354a53
Sachin Sharma <sachinmodgil6@gmail.com>
2022-01-30 11:44:08 +0530

    added login validation

--- files/app.js
+++ files/app.js
@@ -1,9 +1,21 @@
 $("#signupBTN").click(function(){
-    $("#loginform").hide(1000);
-    $("#signupform").show(1000);
+    $("#loginform").hide();
+    $("#signupform").show();
 });
 
 $("#loginBTN").click(function(){
-    $("#loginform").show(1000);
-    $("#signupform").hide(1000);
+    $("#loginform").show();
+    $("#signupform").hide();
+});
+
+$("#login").click(function(){
+    var phnumber = $('#num').val();
+    var password = $('#password').val();
+    if(phnumber && password !== ""){
+        alert("not empty");
+        $("#result").load("/demodata.html");
+    }
+    else{
+        $("#result").html(`<h3 class="text-center">Enter Data First</h3><br/>`);
+    }
 });
